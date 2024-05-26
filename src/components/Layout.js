34fb9c64f2cd28a6de19840407d078f3aa93b396
BrittFranken <160475036+BrittFranken@users.noreply.github.com>
2024-05-26 21:29:58 +0200

    aanpassing layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,15 @@
 import Navbar from './navbar';
 import Footer from './footer';
-
+import styles from '@/styles/Layout.module.css';
 
 export default function Layout({ children }) {
     return (
-      <>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </>
-    )
-  }
+        <>
+            <Navbar />
+            <div className={styles.container}>
+                <main className={styles.main}>{children}</main>
+            </div>
+            <Footer />
+        </>
+    );
+}
