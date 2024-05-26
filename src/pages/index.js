80f058868fe36e9c3d48b12e3da796d273b5de61
BrittFranken <160475036+BrittFranken@users.noreply.github.com>
2024-05-26 21:05:46 +0200

    index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import useNetwork from '@/data/network';
 import Link from 'next/link';
 import styles from '@/styles/Home.module.css';
+import Image from 'next/image';
 
 export default function Home() {
     const [filter, setFilter] = useState('');
@@ -17,19 +18,20 @@ export default function Home() {
     }
 
     return (
-        <div className="background">
-            <h1 className="Hoofd">Zoek hier uw station</h1>
-            <div className="input-container">
+        <div className={styles.background}>
+            <Image src="/Velo-Antwerpen-01.png" alt="Logo" width={200} height={30} className={styles.logo} />
+            <h1 className={styles.hoofd}>Zoek hier uw station</h1>
+            <div className={styles.inputContainer}>
                 <input 
                     type="text" 
                     value={filter} 
                     onChange={handleFilterChange} 
-                    className="input-field"
+                    className={styles.inputField}
                 />
             </div>
             {stations.map(station => (
                 <Link 
-                    className="custom-link" 
+                    className={styles.customLink} 
                     href={`/stations/${station.id}`} 
                     key={station.id}
                 >
