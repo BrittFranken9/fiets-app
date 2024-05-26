b5ec411eafbea95a98b7e45c72f2f2ab01b2c7bb
BrittFranken <160475036+BrittFranken@users.noreply.github.com>
2024-05-26 23:56:16 +0200

    kleine details & footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,21 @@
 import Link from 'next/link';
+import { useState } from 'react';
+import Image from 'next/image';
+import styles from '@/styles/footer.module.css';
 
-export default function footer() {
-    return (
-      <>
-        <footer>
-          <p>Footer</p>
-        </footer>
-      </>
-    )
-  }
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div>
+        <p>Locaties</p>
+        <Image src="/Velo-Antwerpen-01.png" alt="Logo" width={300} height={50}/>
+      </div>
+      <p>© {new Date().getFullYear()} Company Name</p>
+      <p>
+        <Link href="https://www.BRITTvisualdesign.be">
+          BRITTvisualdesign
+        </Link>
+      </p>
+    </footer>
+  );
+}
