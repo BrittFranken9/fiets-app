34a4d89895aebe82c02414d096942c33bd9fbc8f
BrittFranken <160475036+BrittFranken@users.noreply.github.com>
2024-05-27 09:58:49 +0200

    stations

--- src/pages/stations/[stationId].js
+++ src/pages/stations/[stationId].js
@@ -1,19 +1,22 @@
-import useNetwork from '@/data/network'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
+import useNetwork from '@/data/network';
+import styles from '@/styles/StationId.module.css';
+import Image from 'next/image';
 
-export default function Home() {
-    const { network, isLoading, isError } = useNetwork()
-    const router = useRouter()
+export default function StationDetail() {
+  const { network, isLoading, isError } = useNetwork();
+  const router = useRouter();
 
-    if (isLoading) return <div>loading ...</div>;
-    if (isError) return <div>error</div>;
+  if (isLoading) return <div>loading ...</div>;
+  if (isError) return <div>error</div>;
 
-    const station = network.stations.find(station => station.id === router.query.stationId)
+  const station = network.stations.find(station => station.id === router.query.stationId);
 
-    return (
-        <div>
-            <h1>{station.name}</h1>
-            <p>{station.free_bikes} bikes available</p>
-        </div>
-    );
+  return (
+        <div className={styles.stationDetail}>
+            <Image src="/Velo-Antwerpen-02.png" alt="Logo" width={300} height={50} className={styles.logo} />
+        <h1 className={styles.hoofd}>{station.name}</h1>
+        <p>{station.free_bikes} bikes available</p>
+      </div>
+  );
 }
