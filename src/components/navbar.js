6819460befd45f2709cacdf1ca60dedf6c455edc
BrittFranken <160475036+BrittFranken@users.noreply.github.com>
2024-05-28 15:19:49 +0200

    zoek functie

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -14,18 +14,25 @@ export default function Navbar() {
         <nav className={styles.navbar}>
             <div className={styles.logo}>
                 <Link href="/home">
-                  <Image src="/favicon.ico" alt="Logo" width={50} height={0} />
+                    <Image src="/favicon.ico" alt="Logo" width={50} height={50} />
                 </Link>
             </div>
-            <div className={`${styles.hamburger} ${menuOpen ? styles.cross : ''}`} onClick={toggleMenu}>
-                <div className={styles.line}></div>
-                <div className={styles.line}></div>
-                <div className={styles.line}></div>
+            <div className={styles.hamburgerMenuWrapper}>
+                <div className={styles.extraLink}>
+                    <Link href="/zoeken">
+                        <Image src="/zoek.png" alt="Extra Link Photo" width={23} height={21} />
+                    </Link>
+                </div>
+                <div className={`${styles.hamburger} ${menuOpen ? styles.cross : ''}`} onClick={toggleMenu}>
+                    <div className={styles.line}></div>
+                    <div className={styles.line}></div>
+                    <div className={styles.line}></div>
+                </div>
             </div>
             <div className={`${styles.menu} ${menuOpen ? styles.menuOpen : ''}`}>
                 <Link href="/home" onClick={() => setMenuOpen(false)}>Home</Link>
-                <Link href="/about" onClick={() => setMenuOpen(false)}>About</Link>
-                <Link href="/services" onClick={() => setMenuOpen(false)}>Services</Link>
+                <Link href="/about" onClick={() => setMenuOpen(false)}>Over</Link>
+                <Link href="/zoeken" onClick={() => setMenuOpen(false)}>Zoeken</Link>
                 <Link href="/contact" onClick={() => setMenuOpen(false)}>Contact</Link>
             </div>
         </nav>
---
+++ src/pages/zoeken.js
@@ -0,0 +1,73 @@
+import { useState } from 'react';
+import useNetwork from '@/data/network';
+import Link from 'next/link';
+import styles from '@/styles/zoeken.module.css';
+import Image from 'next/image';
+
+export default function Home() {
+    const [filter, setFilter] = useState('');
+    const { network, isLoading, isError } = useNetwork();
+
+    if (isLoading) return <div>loading ...</div>;
+    if (isError) return <div>error</div>;
+
+    function getRelevance(station, filter) {
+        if (station.name.toLowerCase().startsWith(filter.toLowerCase())) {
+            return 1;
+        } else if (station.name.toLowerCase().includes(filter.toLowerCase())) {
+            return 2;
+        }
+        return 3;
+    }
+
+    function removeLeadingDigits(name) {
+        return name.replace(/^\d+\s*-\s*/, '');
+    }
+
+    const filteredStations = filter
+        ? network.stations
+            .filter(station => station.name.toLowerCase().includes(filter.toLowerCase()))
+            .sort((a, b) => getRelevance(a, filter) - getRelevance(b, filter))
+            .slice(0, 5)
+        : [];
+
+    function handleFilterChange(e) {
+        setFilter(e.target.value);
+    }
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <h1 className={styles.hoofd}>Zoek hier uw station</h1>
+                <div className={styles.inputContainer}>
+                    <input 
+                        type="text" 
+                        value={filter} 
+                        onChange={handleFilterChange} 
+                        className={styles.inputField}
+                        placeholder="..."
+                    />
+                </div>
+            </div>
+            <div className={styles.resultsContainer}>
+                <div className={styles.results}>
+                    {filteredStations.map(station => (
+                        <div key={station.id} className={styles.stationContainer}>
+                            <Link 
+                                className={styles.customLink} 
+                                href={`/stations/${station.id}`} 
+                            >
+                                <div className={styles.stationInfo}>
+                                    <span>{removeLeadingDigits(station.name)}</span>
+                                    <span className={`${styles.freeBikes} ${styles.alignRight}`}>
+                                        {station.free_bikes}
+                                    </span>
+                                </div>
+                            </Link>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
